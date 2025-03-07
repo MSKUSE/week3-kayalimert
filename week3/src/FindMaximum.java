5c3ed749e64a14ad67cb991eb4cb4c5aed3abf95
Mertkayali <mertkayali@posta.mu.edu.tr>
2025-03-07 12:02:16 +0300

    S

--- week3/src/FindMaximum.java
+++ week3/src/FindMaximum.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class FindMaximum {
+public class FindMaximum {
+    public static void main(String[] args) {
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        boolean condiditon = num1 > num2;
+        int max = condiditon ? num1 : num2;
+        System.out.println(max);
+
+
+
+
+        int number1=Integer.parseInt(args[0]);
+        int number2=Integer.parseInt(args[1]);
+        if (number1>number2) {
+            System.out.println("Number1 is maximum");
+        }else if (number1<number2) {
+            System.out.println("Number2 is maximum");
+        }else if (number1==number2) {
+            System.out.println("Number1 and Number2 is equal");
+        }
+
+    }
 }
