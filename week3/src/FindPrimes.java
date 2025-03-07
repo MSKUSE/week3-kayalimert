99419c58107b2e407bd242943a5072fd80d6b840
Mertkayali <mertkayali@posta.mu.edu.tr>
2025-03-07 12:06:11 +0300

    A

--- week3/src/FindPrimes.java
+++ week3/src/FindPrimes.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class FindPrimes {
+public class FindPrimes {
+    public static void main(String[] args) {
+        int number = Integer.parseInt(args[0]);
+
+        for (int i = 2; i <= number; i ++){
+            boolean isPrime = true;
+            for (int j = 2; j < i ; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.println(i);
+            }
+        }
+    }
 }
